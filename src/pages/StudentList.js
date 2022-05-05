6b9d88984e729ab91e8046b0a8f05ac399a78af8
Steve Doty <stevedoty16@gmail.com>
2022-05-04 21:11:20 -0700

    ag-grid row selection

--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -6,11 +6,16 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const StudentList = () => {
    const [rowData] = useState([
-       {firstName: 'jo',
-         lastName: 'do',
-         userName: 'yo',
-         schoolName: 'sj',
-         license: 'licensed',}
+    {firstName: 'jo',
+    lastName: 'do',
+    userName: 'yo',
+    schoolName: 'sj',
+    license: 'licensed',},
+    {firstName: 'jo',
+    lastName: 'do',
+    userName: 'yo',
+    schoolName: 'sj',
+    license: 'licensed',},
    ]);
 
    const [columnDefs] = useState([
@@ -21,12 +26,19 @@ const StudentList = () => {
        { field: 'license' }
    ])
 
+   const onSelectionChanged = (grid) => {
+     const selectedRows = grid.api.getSelectedRows();
+console.log(selectedRows);
+   }
+
    return (
        <div className="ag-theme-alpine" style={{height: 400, width: 1000}}>
            <AgGridReact
                rowData={rowData}
-               columnDefs={columnDefs}>
-           </AgGridReact>
+               columnDefs={columnDefs}
+               rowSelection={'multiple'}
+               onSelectionChanged={onSelectionChanged}
+               />
        </div>
    );
 };
